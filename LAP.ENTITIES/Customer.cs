4da4b4d9b1ad457485153876847980e4f8018590
hasansola <sola.hsn54@gmail.com>
2019-09-09 22:06:26 +0300

    Customer ve Order sınıfları eklendi.

---
+++ LAP.ENTITIES/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LAP.ENTITIES
+{
+   public class Customer
+    {
+        [Key]
+        public int InCustomerId { get; set; }
+
+        [Required(ErrorMessage = "Müşteri Isim alanı zorunlu")]
+        [MaxLength(150, ErrorMessage = "Müşteri Isim alanı en fazla 150 karekter olabilir")]
+        public string StName { get; set; }
+
+        public float FlBalance { get; set; }
+
+        [Required(ErrorMessage = "Açıklama alanı zorunlu")]
+        [MaxLength(2000, ErrorMessage = "Açıklama alanı en fazla 2000 karekter olabilir")]
+        public string StDescription { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ LAP.ENTITIES/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace LAP.ENTITIES
+{
+   public class Order
+    {
+        [Key]
+        public int InOrderId { get; set; }
+        [ForeignKey("Customer")]
+        public int InCustomerId { get; set; }
+        public string StProductName { get; set; }
+        public float FlQuantity { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
